78f3f5f84f9c97c6c37de17327ee36bba45d3cbe
Philipp <mike.litoris@gmail.com>
2021-04-21 14:16:36 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/doodlekong/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.plcoding.doodlekong
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
+    }
+}
