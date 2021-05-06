400583de2a6adf468e547e1751d0000b0122f0c5
Philipp <mike.litoris@gmail.com>
2021-05-06 18:11:09 +0200

    Receiving draw data

--- app/src/main/java/com/plcoding/doodlekong/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/plcoding/doodlekong/ui/drawing/DrawingActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.Gravity
 import android.view.MenuItem
+import android.view.MotionEvent
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -18,6 +19,7 @@ import androidx.navigation.navArgs
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import com.plcoding.doodlekong.R
+import com.plcoding.doodlekong.data.remote.ws.models.DrawAction
 import com.plcoding.doodlekong.data.remote.ws.models.GameError
 import com.plcoding.doodlekong.data.remote.ws.models.JoinRoomHandshake
 import com.plcoding.doodlekong.databinding.ActivityDrawingBinding
@@ -53,6 +55,8 @@ class DrawingActivity : AppCompatActivity() {
         toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
         toggle.syncState()
 
+        binding.drawingView.roomName = args.roomName
+
         val header = layoutInflater.inflate(R.layout.nav_drawer_header, binding.navView)
         rvPlayers = header.findViewById(R.id.rvPlayers)
         binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
@@ -73,6 +77,13 @@ class DrawingActivity : AppCompatActivity() {
 
             override fun onDrawerStateChanged(newState: Int) = Unit
         })
+
+        binding.ibUndo.setOnClickListener {
+            if(binding.drawingView.isUserDrawing) {
+                binding.drawingView.undo()
+                viewModel.sendBaseModel(DrawAction(DrawAction.ACTION_UNDO))
+            }
+        }
 
         binding.colorGroup.setOnCheckedChangeListener { _, checkedId ->
             viewModel.checkRadioButton(checkedId)
@@ -125,6 +136,25 @@ class DrawingActivity : AppCompatActivity() {
     private fun listenToSocketEvents() = lifecycleScope.launchWhenStarted {
         viewModel.socketEvent.collect { event ->
             when(event) {
+                is DrawingViewModel.SocketEvent.DrawDataEvent -> {
+                    val drawData = event.data
+                    if(!binding.drawingView.isUserDrawing) {
+                        when(drawData.motionEvent) {
+                            MotionEvent.ACTION_DOWN -> binding.drawingView.startedTouchExternally(
+                                drawData
+                            )
+                            MotionEvent.ACTION_MOVE -> binding.drawingView.movedTouchExternally(
+                                drawData
+                            )
+                            MotionEvent.ACTION_UP -> binding.drawingView.releasedTouchExternally(
+                                drawData
+                            )
+                        }
+                    }
+                }
+                is DrawingViewModel.SocketEvent.UndoEvent -> {
+                    binding.drawingView.undo()
+                }
                 is DrawingViewModel.SocketEvent.GameErrorEvent -> {
                     when(event.data.errorType) {
                         GameError.ERROR_ROOM_NOT_FOUND -> finish()
--- app/src/main/java/com/plcoding/doodlekong/ui/views/DrawingView.kt
+++ app/src/main/java/com/plcoding/doodlekong/ui/views/DrawingView.kt
@@ -39,6 +39,7 @@ class DrawingView @JvmOverloads constructor(
     private var paths = Stack<PathData>()
     private var pathDataChangedListener: ((Stack<PathData>) -> Unit)? = null
 
+    private var startedTouch = false
     var roomName: String? = null
     var isUserDrawing = false
         set(value) {
@@ -80,6 +81,61 @@ class DrawingView @JvmOverloads constructor(
             strokeWidth = initialThickness
         }
         canvas?.drawPath(path, paint)
+    }
+
+    fun startedTouchExternally(drawData: DrawData) {
+        parseDrawData(drawData).apply {
+            paint.color = color
+            paint.strokeWidth = thickness
+            path.reset()
+            path.moveTo(fromX, fromY)
+            invalidate()
+            startedTouch = true
+        }
+    }
+
+    fun movedTouchExternally(drawData: DrawData) {
+        parseDrawData(drawData).apply {
+            val dx = abs(toX - fromX)
+            val dy = abs(toY - fromY)
+            if(!startedTouch) {
+                startedTouchExternally(drawData)
+            }
+            if(dx >= smoothness || dy >= smoothness) {
+                path.quadTo(fromX, fromY, (fromX + toX) / 2f, (fromY + toY) / 2f)
+                invalidate()
+            }
+        }
+    }
+
+    fun releasedTouchExternally(drawData: DrawData) {
+        parseDrawData(drawData).apply {
+            path.lineTo(fromX, fromY)
+            canvas?.drawPath(path, paint)
+            paths.push(PathData(path, paint.color, paint.strokeWidth))
+            pathDataChangedListener?.let { change ->
+                change(paths)
+            }
+            path = Path()
+            invalidate()
+            startedTouch = false
+        }
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        path.reset()
+        invalidate()
+    }
+
+    fun undo() {
+        if(paths.isNotEmpty()) {
+            paths.pop()
+            pathDataChangedListener?.let { change ->
+                change(paths)
+            }
+            invalidate()
+        }
     }
 
     private fun startedTouch(x: Float, y: Float) {
@@ -137,6 +193,15 @@ class DrawingView @JvmOverloads constructor(
             ACTION_UP -> releasedTouch()
         }
         return true
+    }
+
+    private fun parseDrawData(drawData: DrawData): DrawData {
+        return drawData.copy(
+            fromX = drawData.fromX * viewWidth!!,
+            fromY = drawData.fromY * viewHeight!!,
+            toX = drawData.toX * viewWidth!!,
+            toY = drawData.toY * viewHeight!!
+        )
     }
 
     private fun createDrawData(
